dfe5f2757d14d0f55a24f90c9573bc4ca3af0150
shinhugh <hughbee135@gmail.com>
2018-11-28 23:06:26 -0800

    Edit main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
  */
 int main() {
 
-  ExpMax em("temporary", "temporary", "temporary", "temporary");
-  em.mostProbVal("temporary", "temporary");
+  ExpMax em("initY.txt", "initX.txt", "data.txt", "meanings.txt");
+  std::cout << "David is most likely to "
+    << em.mostProbVal("David", "Book3") << " Book 3.\n";
 
   return 0;
 }
