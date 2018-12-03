fffebe592bdf86e374c2f1bb7c96bde395c5fd5f
Sheldon <sheldonlau1@gmail.com>
2018-12-03 11:16:40 -0800

    hard code question order, remove user input for deciding number of files

--- main.cpp
+++ main.cpp
@@ -28,30 +28,29 @@ int main(int argc, char **argv)
   std::string data_file = fileDir + IN_DIR + fileSetName + DATA;
   std::string meanings_file = fileDir + IN_DIR + fileSetName + MEANINGS;
 
-  int numFiles = 4;
+  // int responseNum = -1;
+  // while (!(responseNum == 0 || responseNum == 1))
+  // {
+  //   std::cout << "Please choose what file inputs to use.\n";
+  //   std::cout << "(0) 4 files (data, initX, initY, meanings)\n";
+  //   std::cout << "(1) 2 files (data, meanings)\n";
+  //   std::cin >> responseNum;
+  //   if (std::cin.fail())
+  //   {
+  //     std::cin.clear();
+  //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  //     responseNum = -1;
+  //   }
+  // }
+
+  // ExpMax em(initY_file, initX_file, data_file, meanings_file);
+  // if (responseNum == 1)
+  // {
+  //   numFiles = 2;
+  //   ExpMax em(4, data_file, meanings_file);
+  // }
+  ExpMax em(4, data_file, meanings_file);
   int responseNum = -1;
-  while (!(responseNum == 0 || responseNum == 1))
-  {
-    std::cout << "Please choose what file inputs to use.\n";
-    std::cout << "(0) 4 files (data, initX, initY, meanings)\n";
-    std::cout << "(1) 2 files (data, meanings)\n";
-    std::cin >> responseNum;
-    if (std::cin.fail())
-    {
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      responseNum = -1;
-    }
-  }
-
-  ExpMax em(initY_file, initX_file, data_file, meanings_file);
-  if (responseNum == 1)
-  {
-    numFiles = 2;
-    ExpMax em(4, data_file, meanings_file);
-  }
-
-  responseNum = -1;
   while (!(responseNum == 0 || responseNum == 1))
   {
     std::cout << "Would you like to take the survey or view the results?\n";
@@ -130,6 +129,13 @@ int main(int argc, char **argv)
                   << selectedCategory << " goes well with " << result.second
                   << " moods.\n\n";
       }
+      do
+      {
+        std::cout << '\n'
+                  << "Press enter to continue...";
+      } while (std::cin.get() != '\n');
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
   else
@@ -139,7 +145,54 @@ int main(int argc, char **argv)
     std::cin >> inputName;
 
     std::vector<std::string> names = em.getSampleNames();
-    std::vector<std::string> categories = {"Pop","Hip Hop","Alternative","Metal","Jazz","Classical","House","Trap","Future Bass","Drumstep"};
+    std::vector<std::string> categories = {"Test the quality of parts before shipment",
+                                           "Study the structure of the human body",
+                                           "Conduct a musical choir",
+                                           "Give career guidance to people",
+                                           "Sell restaurant franchises to individuals",
+                                           "Generate the monthly payroll checks for an office",
+                                           "Lay brick or tile",
+                                           "Study animal behavior",
+                                           "Direct a play",
+                                           "Do volunteer work at a non-profit organization",
+                                           "Sell merchandise at a department store",
+                                           "Inventory supplies using a hand-held computer",
+                                           "Work on an offshore oil-drilling rig",
+                                           "Do research on plants or animals",
+                                           "Design artwork for magazines",
+                                           "Help people who have problems with drugs or alcohol",
+                                           "Manage the operations of a hotel",
+                                           "Use a computer program to generate customer bills",
+                                           "Assemble electronic parts",
+                                           "Develop a new medical treatment or procedure",
+                                           "Write a song",
+                                           "Teach an individual an exercise routine",
+                                           "Operate a beauty salon or barber shop",
+                                           "Maintain employee records",
+                                           "Operate a grinding machine in a factory",
+                                           "Conduct biological research",
+                                           "Write books or plays",
+                                           "Help people with family-related problems",
+                                           "Manage a department within a large company",
+                                           "Compute and record statistical and other numerical data",
+                                           "Fix a broken faucet",
+                                           "Study whales and other types of marine life",
+                                           "Play a musical instrument",
+                                           "Supervise the activities of children at a camp",
+                                           "Manage a clothing store",
+                                           "Operate a calculator",
+                                           "Assemble products in a factory",
+                                           "Work in a biology lab",
+                                           "Perform stunts for a movie or television show",
+                                           "Teach children how to read",
+                                           "Sell houses",
+                                           "Handle customers bank transactions",
+                                           "Install flooring in houses",
+                                           "Make a map of the bottom of an ocean",
+                                           "Design sets for plays",
+                                           "Help elderly people with their daily activities",
+                                           "Run a toy store",
+                                           "Keep shipping and receiving records"};
     std::vector<int> userInputs;
     std::vector<std::string> inputMeanings = em.getInputNames();
     for (int i = 0; i < categories.size(); i++)
@@ -186,11 +239,6 @@ int main(int argc, char **argv)
     }
     outfile2 << outMsg;
     outfile2.close();
-    ExpMax em(initY_file, initX_file, data_file, meanings_file);
-    if (numFiles == 2)
-    {
-      ExpMax em(4, data_file, meanings_file);
-    }
 
     for (int i = 0; i < categories.size(); i++)
     {
